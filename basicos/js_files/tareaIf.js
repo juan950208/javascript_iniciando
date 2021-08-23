7b3aebda960e878a7b0fe77c9e14ed30618fdd69
juan950208 <jglc1995@gmail.com>
2021-08-22 19:21:38 -0500

    ejercicios operador if

---
+++ basicos/js_files/tareaIf.js
@@ -0,0 +1,8 @@
+let answer = prompt('cual es el nombre "oficial" de javascript', 'escriba aqui su respuesta');
+
+if(answer == 'ECMAScript'){
+    alert('CORRECTO');
+}else{
+    alert('INCORRECTO');
+    alert('el nombre es ECMAScript');
+}
